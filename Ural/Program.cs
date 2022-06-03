66d71513af5e97c01879092a199c3706d5301d45
RusGol718 <ruslanf743@gmail.com>
2022-06-03 13:21:59 +0100

    Encoding UTF-16
    Changed from ASCII to UTF-16

--- Ural/Program.cs
+++ Ural/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Ural.Ural_translator_compliler.Datatypes.Numbers;
+using Ural.Ural_translator_compliler.Datatypes.Booleans;
 using Ural.Ural_translator_compliler.Datatypes.Strings;
 using Ural.Ural_translator_compliler.Datatypes;
 
@@ -17,11 +18,17 @@ namespace Ural
             Token test = new Token("INT", "5");
             Error test_error = new SyntaxInvalidCharacterError(6, 9);
 
-            str32 val = new str32('G');
+
+            str32 val1 = new str32('G');
+            natch64 val2 = new natch64(9);
+            RCI val3 = new RCI(true);
+
 
             //Console.WriteLine(test.value, Token.digit_const_str);
-            Console.WriteLine($"{val.value}, {val.value.GetType()}");
+            Console.WriteLine($"{val3.value}, {val3.value.GetType()}");
             //Console.ReadKey();
         }
     }
+
+    
 }
