740ff3683cef4a9df949f8a2dd09b7d7e5fba498
RusGol718 <ruslanf743@gmail.com>
2022-06-29 17:19:57 +0100

    V2.0.11.1
    Created True False/ ISTINA LOZH classes

--- Ural/Program.cs
+++ Ural/Program.cs
@@ -34,8 +34,9 @@ namespace Ural
             Console.WriteLine(num3.divide(num4).value);
             */
             drobch64 num5 = new drobch64(8.0D);
-
+            int? x = (int?)6.9D;
             Console.WriteLine(num5);
+            Console.WriteLine(x);
             Console.WriteLine(num5.GetType());
             Console.WriteLine(num5.value.GetType());
             Console.WriteLine(num5.value);
---
+++ Ural/Ural_translator_compliler/Datatypes/Booleans/LOZH.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Ural.Ural_translator_compliler.Datatypes.Booleans
+{
+    public class LOZH : RCI
+    {
+
+        public LOZH(string _value) : base(_value)
+        {
+            if (_value == "ИСТИНА") //TRUE
+            {
+                this.value = _value;
+            }
+            else
+            {
+                //raise error;
+            }
+        }
+
+        public LOZH(bool _value) : base(_value)
+        {
+            if (_value)
+            {
+                this.value = "ИСТИНА"; //TRUE
+            }
+            else if (!_value)
+            {
+                this.value = "ЛОЖЬ"; //FALSE
+            }
+            else
+            {
+                //Raise error
+            }
+        }
+
+    }
+}
