45704c300178472f2ccaf4250d12e053da7b1028
RusGol718 <ruslanf743@gmail.com>
2022-07-31 12:55:05 +0100

    V2.1.5.1
    Testing a negative Natch Error in Main.cs

--- Ural/Main.cs
+++ Ural/Main.cs
@@ -19,6 +19,8 @@ namespace Ural
         {
             celch64 num1 = new celch64(8);
             celch64 num2 = new celch64(4);
+            NatchOutOfRangeOtricError er1 = new NatchOutOfRangeOtricError(0, 0);
+            Console.WriteLine(er1.error_name + er1.discription);
             Console.WriteLine(num1.divide(num2).value);
             Console.ReadKey();
         }
