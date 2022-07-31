4734b5343030888e7e21a771c88e71cb1c3469b6
RusGol718 <ruslanf743@gmail.com>
2022-07-31 12:27:14 +0100

    V2.1.5.0
    Added All Numeric Out Of Range Errors as datatypes

--- Ural/Main.cs
+++ Ural/Main.cs
@@ -7,6 +7,8 @@ using Ural.Ural_translator_compliler.Datatypes;
 
 using Ural.Ural_translator_compliler.Errors;
 using Ural.Ural_translator_compliler.Errors.SyntaxInvalidError;
+using Ural.Ural_translator_compliler.Errors.SyntaxInvalidError.ErrorDatatypes;
+using Ural.Ural_translator_compliler.Errors.SyntaxInvalidError.ErrorDatatypes.ErrorOutOfRange;
 using Ural.Ural_translator_compliler.Operations;
 using Ural.Ural_translator_compliler;
 namespace Ural
@@ -17,7 +19,7 @@ namespace Ural
         {
             celch64 num1 = new celch64(8);
             celch64 num2 = new celch64(4);
-            Console.WriteLine(num1.divide(num2));
+            Console.WriteLine(num1.divide(num2).value);
             Console.ReadKey();
         }
     }
--- Ural/Ural_translator_compliler/Errors/SyntaxInvalidError/ErrorDatatypes/SyntaxInvalidDrobchDatatypeError.cs
+++ Ural/Ural_translator_compliler/Errors/SyntaxInvalidError/ErrorDatatypes/SyntaxInvalidDrobchDatatypeError.cs
@@ -5,7 +5,7 @@ namespace Ural.Ural_translator_compliler.Errors.SyntaxInvalidError.ErrorDatatype
 {
     public class SyntaxInvalidDrobchDatatypeError : Error
     {
-        public SyntaxInvalidDrobchDatatypeError(int _position_start, int _position_end, string _error_name = "Синтаксическая ошибка: неверное дробное число.", string _discription = "Синтаксическая ошибка при некорректном создании дробных чисел.") : base(_position_start, _position_end, _error_name, _discription)
+        public SyntaxInvalidDrobchDatatypeError(int _position_start, int _position_end, string _error_name = "Синтаксическая ошибка: неверное дробное число. ", string _discription = "Синтаксическая ошибка при некорректном создании дробных чисел.") : base(_position_start, _position_end, _error_name, _discription)
         {
             // Inherited Constructor
         }
