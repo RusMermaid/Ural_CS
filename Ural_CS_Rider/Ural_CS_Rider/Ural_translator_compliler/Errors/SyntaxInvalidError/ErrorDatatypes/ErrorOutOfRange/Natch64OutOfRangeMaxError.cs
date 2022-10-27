3c1c004aec40b39dcd288c2aa820b751b9f17c71
RusGol718 <ruslanf743@gmail.com>
2022-10-27 16:12:04 +0300

    V2.1.0.1 RIDER

---
+++ Ural_CS_Rider/Ural_CS_Rider/Ural_translator_compliler/Errors/SyntaxInvalidError/ErrorDatatypes/ErrorOutOfRange/Natch64OutOfRangeMaxError.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Ural.Ural_translator_compliler.Errors.SyntaxInvalidError.ErrorDatatypes.ErrorOutOfRange
+{
+    public class Natch64OutOfRangeMaxError : SyntaxInvalidNatchDatatypeError
+    {
+        public Natch64OutOfRangeMaxError(int _position_start, int _position_end, string _error_name = "Натуральное число не в диапазоне: ", string _discription = "число не должно превышать 18446744073709551615.") : base(_position_start, _position_end, _error_name, _discription)
+        {
+
+        }
+    }
+}
+
---
+++ Ural_CS_Rider/Ural_CS_Rider/Ural_translator_compliler/Location.cs
@@ -0,0 +1,12 @@
+//V2.0.5
+using System;
+namespace Ural.Ural_translator_compliler
+
+{
+  public class Location
+  {
+    public Location()
+    {
+    }
+  }
+}
