a6148116af3526184b13cc7e233bb079383e7322
Degreezee <76453732+Degreezee@users.noreply.github.com>
2022-11-10 20:24:01 +0300

    V2.1.5.1
    Overloaded TaylorFunction/Gamma/Factorial/DvoynoyFactorial/ProstyeChisla/CheckProstoe/Proizvodnaya/VtorayaProizvodnaya/TretyaProizvodnaya/NProizvodnaya.
    
    Added UralSystem.
    
    Co-Authored-By: Ruslan Golov <92940400+RusMermaid@users.noreply.github.com>

---
+++ Ural_CS_Rider/Ural_CS_Rider/Ural_translator_compiler/Libraries/UralMathLib/UralSystem.cs
@@ -0,0 +1,168 @@
+﻿using System.IO.Enumeration;
+
+namespace Ural_CS_Rider.Ural_translator_compiler.Libraries.UralMathLib
+{
+    using System;
+    using System.Buffers;
+    using System.Buffers.Binary;
+    using System.Buffers.Text;
+    
+    using System.Collections;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Collections.Immutable;
+    using System.Collections.Specialized;
+    using System.Collections.ObjectModel;
+    
+    using System.Configuration;
+    using System.Configuration.Assemblies;
+    using System.Configuration.Internal;
+    using System.Configuration.Provider;
+    
+    using System.Data;
+    using System.Data.Common;
+    using System.Data.Odbc;
+    using System.Data.Sql;
+    using System.Data.OleDb;
+    using System.Data.SqlClient;
+    using System.Data.SqlTypes;
+    
+    using System.Diagnostics;
+    using System.Diagnostics.Contracts;
+    using System.Diagnostics.Eventing;
+    using System.Diagnostics.Metrics;
+    using System.Diagnostics.Tracing;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Diagnostics.Eventing;
+    using System.Diagnostics.Eventing.Reader;
+    using System.Diagnostics.PerformanceData;
+    using System.Diagnostics.SymbolStore;
+    
+    using System.Drawing;
+    using System.Drawing.Configuration;
+    using System.Drawing.Design;
+    using System.Drawing.Imaging;
+    using System.Drawing.Printing;
+    using System.Drawing.Text;
+    using System.Drawing.Drawing2D;
+    
+    using System.Dynamic;
+    
+    using System.Formats;
+    using System.Formats.Asn1;
+    
+    using System.Globalization;
+    
+    using System.Linq;
+    using System.Linq.Expressions;
+    
+    using System.Media;
+    
+    using System.Net;
+    using System.Net.Cache;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Net.Http.Json;
+    using System.Net.Mail;
+    using System.Net.Mime;
+    using System.Net.Security;
+    using System.Net.Sockets;
+    using System.Net.NetworkInformation;
+    using System.Net.NetworkInformation;
+    using System.Net.WebSockets;
+    using System.Net.PeerToPeer;
+    using System.Net.PeerToPeer.Collaboration;
+    
+    using System.Numerics;
+    
+    using System.Reflection;
+    using System.Reflection.Emit;
+    using System.Reflection.Metadata;
+    using System.Reflection.Metadata.Ecma335;
+    using System.Reflection.PortableExecutable;
+    
+    using System.Resources;
+    
+    using System.Runtime;
+    using System.Runtime.Intrinsics;
+    using System.Runtime.Intrinsics.Arm;
+    using System.Runtime.Intrinsics.X86;
+    using System.Runtime.Loader;
+    using System.Runtime.Remoting;
+    using System.Runtime.Serialization;
+    using System.Runtime.Serialization.Formatters;
+    using System.Runtime.Serialization.Formatters.Binary;
+    using System.Runtime.Versioning;
+    using System.Runtime.CompilerServices;
+    using System.Runtime.ConstrainedExecution;
+    using System.Runtime.ExceptionServices;
+    using System.Runtime.InteropServices;
+    using System.Runtime.InteropServices.ComTypes;
+    using System.Runtime.InteropServices.ObjectiveC;
+    
+    using System.Security;
+    using System.Security.Authentication;
+    using System.Security.Authentication.ExtendedProtection;
+    using System.Security.Claims;
+    using System.Security.Cryptography;
+    using System.Security.Cryptography.Pkcs;
+    using System.Security.Cryptography.Xml;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Security.Permissions;
+    using System.Security.Policy;
+    using System.Security.Principal;
+    using System.Security.AccessControl;
+    
+    using System.Text;
+    using System.Text.Encodings;
+    using System.Text.Encodings.Web;
+    using System.Text.Json;
+    using System.Text.Json.Nodes;
+    using System.Text.Json.Serialization;
+    using System.Text.Json.Serialization.Metadata;
+    using System.Text.Unicode;
+    using System.Text.RegularExpressions;
+    
+    using System.Threading;
+    using System.Threading.Channels;
+    using System.Threading.Tasks;
+    using System.Threading.Tasks.Dataflow;
+    using System.Threading.Tasks.Sources;
+    
+    using System.Timers;
+    
+    using System.Transactions;
+    
+    using System.Web;
+    
+    using System.Windows;
+    using System.Windows.Input;
+    using System.Windows.Markup;
+    
+    using System.Xml;
+    using System.Xml.Linq;
+    using System.Xml.Resolvers;
+    using System.Xml.Schema;
+    using System.Xml.Serialization;
+    using System.Xml.Xsl;
+    using System.Xml.XPath;
+    
+    using System.CodeDom;
+    using System.CodeDom.Compiler;
+    
+    using System.ComponentModel;
+    using System.ComponentModel.Design;
+    using System.ComponentModel.Design.Serialization;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    
+    using System.IO;
+    using System.IO.Compression;
+    using System.IO.Enumeration;
+    using System.IO.Packaging;
+    using System.IO.Pipes;
+    using System.IO.Ports;
+    using System.IO.IsolatedStorage;
+    using System.IO.MemoryMappedFiles;
+}
