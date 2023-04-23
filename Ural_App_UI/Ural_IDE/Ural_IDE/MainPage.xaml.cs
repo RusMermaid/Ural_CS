c866e592cbde3ea822d148df3cca342eadf923ee
RusMermaid <ruslanf743@gmail.com>
2023-04-23 18:03:58 +0300

    V2.2.3.4.1
    Linked Main function and UralSystem to MacOS and Android emulator apps

--- Ural_App_UI/Ural_IDE/Ural_IDE/MainPage.xaml.cs
+++ Ural_App_UI/Ural_IDE/Ural_IDE/MainPage.xaml.cs
@@ -1,8 +1,15 @@
-﻿namespace Ural_IDE;
+﻿
+using Ural_CS_Rider;
+
+namespace Ural_IDE;
+
 
 public partial class MainPage : ContentPage
 {
 	int count = 0;
+
+
+
 
 	public MainPage()
 	{
@@ -11,14 +18,27 @@ public partial class MainPage : ContentPage
 
 	private void OnCounterClicked(object sender, EventArgs e)
 	{
-		count++;
+        matrica c = new matrica(2);
+        c[1, 1] = new drobch64(8);
+        c[1, 0] = new drobch64(-9);
+        short n = 60;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+        matrica d = new matrica(3);
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
+        d[0, 0] = new drobch64(2);
+        d[0, 1] = new drobch64(-2);
+        d[1, 0] = new drobch64(4);
+        d[1, 1] = new drobch64(6);
+        d[2, 1] = new drobch64(10);
+        d[2, 2] = new drobch64(2);
+        d[2, 0] = new drobch64(-10);
+        // StrIndexOutOfRangeError err = new StrIndexOutOfRangeError(0, 0);
+        //err.Execute();
+
+        label2.Text = MainClass.Main();
+        //CounterBtn.Text  = MainClass.Main();
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
 	}
 }
 
---
+++ Ural_App_UI/Ural_IDE/Ural_IDE/UralSystem/UralCustomDatatypes.cs
@@ -0,0 +1,10 @@
+﻿global using Datatypes;
+global using Datatypes.Booleans;
+global using Datatypes.Numbers;
+global using Datatypes.Numbers.OtherNumbers;
+global using Datatypes.Numbers.OtherNumbers.Binary;
+global using Datatypes.Numbers.OtherNumbers.Octal;
+global using Datatypes.Numbers.OtherNumbers.Hexadecimal;
+global using Datatypes.Strings;
+global using Datatypes.Collections;
+global using Datatypes.Collections.MathCollections;
