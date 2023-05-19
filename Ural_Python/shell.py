50a16ee63fe47ca15d59e84d691e3c251f23e465
RusMermaid <ruslanf743@gmail.com>
2023-05-19 20:24:34 +0300

    V2.2.3.9.2
    Unused Python scripts and classes removed

--- Ural_Python/shell.py
+++
@@ -1,9 +0,0 @@
-from ural_interpreter import ural
-
-while True:
-    text = input("Saturn  -> \n")
-    result_code, error = ural.run("<strdin>", text)
-    if error:
-        print(error.__repr__())
-    else:
-        print(result_code)
--- Ural_Python/ural_interpreter/num_node.py
+++
@@ -1,6 +0,0 @@
-class NumberNode:
-    def __init__(self, token):
-        self.token = token
-
-    def __repr__(self):
-        return f"{self.token}"
