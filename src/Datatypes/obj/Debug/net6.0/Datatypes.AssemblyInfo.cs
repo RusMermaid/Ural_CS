bfeefcc7264dc548c68ecf7962d481e9eb21cf23
RusMermaid <ruslanf743@gmail.com>
2024-09-22 18:55:31 +0300

    V2.2.4.4.19
    Updated Lambda parcing in main to test coursework hypothesis

--- src/Datatypes/obj/Debug/net6.0/Datatypes.AssemblyInfo.cs
+++ src/Datatypes/obj/Debug/net6.0/Datatypes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Datatypes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e4ff1a986da38e46a7cf07acecd5e7d6752b602")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa78995339962f4dc9fde9e2f9ec4928c668ba01")]
 [assembly: System.Reflection.AssemblyProductAttribute("Datatypes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Datatypes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/UralMathFsharpLib/UralMathFsharpLib/obj/Debug/net6.0/UralMathFsharpLib.AssemblyInfo.fs
+++ src/UralMathFsharpLib/UralMathFsharpLib/obj/Debug/net6.0/UralMathFsharpLib.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("UralMathFsharpLib")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e4ff1a986da38e46a7cf07acecd5e7d6752b602")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa78995339962f4dc9fde9e2f9ec4928c668ba01")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("UralMathFsharpLib")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("UralMathFsharpLib")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
